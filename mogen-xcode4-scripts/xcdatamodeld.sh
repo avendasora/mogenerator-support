7ccc222386f7ae535f7ac021d2280095de78c9c9
David Avendasora <avendasora@me.com>
2011-10-31 14:14:52 +0800

    Initial Commit of mogenerator templates and scripts to trigger mogeneration when a project is built.

---
+++ mogen-xcode4-scripts/xcdatamodeld.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#  mogend.sh
+#
+#  Created by David Avendasora on 10/31/2011
+#  Based on the scripts created by Jean-Denis Muys
+
+$echo ${SCRIPT_INPUT_FILE_0}
+
+baseClass=KESManagedObject
+echo $baseClass
+
+templatePath=${SOURCE_ROOT}/../mogenerator-support/mogen-templates
+echo $templatePath
+
+basePath=${SOURCE_ROOT}/${PROJECT_NAME}
+echo $basePath
+
+humanDir=${basePath}/ModelClasses
+echo $humanDir
+
+machineDir=${humanDir}/MOGenerated
+echo $machineDir
+
+currentModelVersion=`/usr/libexec/PlistBuddy "${SCRIPT_INPUT_FILE_0}/.xccurrentversion" -c 'print _XCCurrentVersionName'`
+echo $currentModelVersion
+
+echo mogenerator --model \"${SCRIPT_INPUT_FILE_0}/$currentModelVersion\" --base-class \"${baseClass}\" --template-path \"${templatePath}\" --machine-dir \"${machineDir}\" --human-dir \"${humanDir}\"
+mogenerator --model "${SCRIPT_INPUT_FILE_0}/$currentModelVersion" --base-class "${baseClass}" --template-path "${templatePath}" --machine-dir "${machineDir}" --human-dir "${humanDir}"
